bba93b65c66f3503ce956caac1e29ae08db70439
Pablo Alexander <zleepy2002@gmail.com>
2025-12-02 14:18:47 -0600

    awsfsrdgv

---
+++ Assets/Game/Scripts/ScoreScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+public class ScoreScript : MonoBehaviour
+{
+    public static ScoreScript instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
